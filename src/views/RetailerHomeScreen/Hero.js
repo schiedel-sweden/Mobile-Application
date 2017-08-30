368be17e7c88e4050471e5158adc1ec66d121361
Jeremy Danner <jeremydanner2@gmail.com>
2017-08-30 14:10:39 +0200

    fixed border color

--- src/views/RetailerHomeScreen/Hero.js
+++ src/views/RetailerHomeScreen/Hero.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         alignSelf: 'stretch',
         borderBottomWidth: 2,
-        borderColor: '#000',
+        borderColor: '#333333',
         flex: 0.5,
         justifyContent: 'center',
     },
