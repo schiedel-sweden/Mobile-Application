430a39afd75c8a6434c2c607ef14eec9f982e39c
Jonathan Partain <jonathan.partain@gmail.com>
2017-09-01 15:35:25 +0200

    Moved configs to one file, tab-icons now change depending on if they are focused or not

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {TabNavigator} from 'react-navigation';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Image} from 'react-native';
 
 import AboutUs from './src/views/AboutUs/AboutUs.js';
 import ContactUs from './src/views/ContactUs/ContactUs.js';
@@ -8,16 +8,82 @@ import PriceSuggestion from './src/views/PriceSuggestion/PriceSuggestion.js';
 import Products from './src/views/Products/Products.js';
 import Retailers from './src/views/RetailersView/RetailersView.js';
 
-const Hej = TabNavigator({
-    Retailers: { screen: Retailers },
-    PriceSuggestion: { screen: PriceSuggestion },
-    Products: { screen: Products },
-    AboutUs: { screen: AboutUs },
-    ContactUs: { screen: ContactUs },
+const iconStyle = require('./src/styles/navStyles');
+
+const App = TabNavigator({
+    Retailers: { screen: Retailers,
+        navigationOptions: {
+            tabBarLabel: 'Återförsäljare',
+            tabBarIcon: ({ focused }) => {
+                  let imgSource = focused ? require('./src/images/icons/aterforsaljare_focus.png') : require('./src/images/icons/aterforsaljare.png');
+
+                  return <Image
+                    source={imgSource}
+                    style={iconStyle.icon}
+                  />
+              },
+        },
+    },
+
+    PriceSuggestion: { screen: PriceSuggestion,
+        navigationOptions: {
+            tabBarLabel: 'Prisförslag',
+            tabBarIcon: ({ focused }) => {
+                  let imgSource = focused ? require('./src/images/icons/prisforslag_focus.png') : require('./src/images/icons/prisforslag.png');
+
+                  return <Image
+                    source={imgSource}
+                    style={iconStyle.icon}
+                  />
+              },
+        },
+    },
+    Products: { screen: Products,
+        navigationOptions: {
+            tabBarLabel: 'Produkter',
+            tabBarIcon: ({ focused }) => {
+                  let imgSource = focused ? require('./src/images/icons/produkter_focus.png') : require('./src/images/icons/produkter.png');
+
+                  return <Image
+                    source={imgSource}
+                    style={iconStyle.icon}
+                  />
+              },
+        },
+    },
+
+    AboutUs: { screen: AboutUs,
+
+        navigationOptions: {
+            tabBarLabel: 'Om oss',
+            tabBarIcon: ({ focused }) => {
+                  let imgSource = focused ? require('./src/images/icons/om-oss_focus.png') : require('./src/images/icons/om-oss.png');
+
+                  return <Image
+                    source={imgSource}
+                    style={iconStyle.icon}
+                  />
+              },
+        },
+    },
+
+    ContactUs: { screen: ContactUs,
+        navigationOptions: {
+            tabBarLabel: 'Kontakta oss',
+            tabBarIcon: ({ focused }) => {
+                  let imgSource = focused ? require('./src/images/icons/kontakta-oss_focus.png') : require('./src/images/icons/kontakta-oss.png');
+
+                  return <Image
+                    source={imgSource}
+                    style={iconStyle.icon}
+                  />
+              },
+        },
+    },
 },
 {
     tabBarPosition: 'bottom'
 }
 );
 
-export default Hej;
+export default App;
--- src/views/ContactUs/ContactUs.js
+++ src/views/ContactUs/ContactUs.js
@@ -6,24 +6,13 @@ import {
     StyleSheet,
 } from 'react-native';
 
-const iconStyle = require('../../styles/navStyles');
-
 export default class ContactUs extends React.Component {
-    static navigationOptions = {
-        title: 'Kontakta oss',
-        tabBarIcon: ({ tintColor }) => (
-          <Image
-            source={require('../../images/icons/kontakta-oss.png')}
-            style={[iconStyle.icon]}
-          />
-      ),
-    };
 
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
-                <Text>Kontakta oss my dude</Text>
+            <Text>Kontakta oss my dude</Text>
             </View>
         );
     }
