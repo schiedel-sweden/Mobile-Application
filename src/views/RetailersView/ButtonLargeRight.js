74798bc9e794f29674a56f836c90d3966a2c7a2d
Jeremy Danner <jeremydanner2@gmail.com>
2017-09-01 14:51:13 +0200

    added buttons and text to retailers

--- src/views/RetailersView/ButtonLargeRight.js
+++ src/views/RetailersView/ButtonLargeRight.js
@@ -3,17 +3,31 @@ import {
     StyleSheet,
     Text,
     View,
+    Image,
 } from 'react-native';
 
 export default class ButtonLargeRight extends React.Component {
-    handleClick(){
-        alert('Right button clicked!');
-    }
     render() {
+
         return(
             <View style={styles.container}>
-                <Text style={styles.text}>Hello from larg button Right</Text>
-                <Text style={styles.text}>More text</Text>
+                <View style={styles.wrapper}>
+                    <Image
+                      source={require('../../images/icons/produkter.png')}
+                      style={styles.icon}
+                    />
+                </View>
+                <View style={styles.wrapper}>
+                    <Text style={styles.h2}>
+                        Produkter
+                    </Text>
+                </View>
+                <View style={styles.wrapper}>
+                    <Text style={styles.h4}>
+                        Kolla in vårt sortiment där du kan finna alla våra
+                        skorstnar och
+                    </Text>
+                </View>
             </View>
         );
     }
@@ -22,11 +36,30 @@ export default class ButtonLargeRight extends React.Component {
 const styles = StyleSheet.create({
     container: {
         borderColor: '#333333',
-        borderLeftWidth: 1,
+        borderRightWidth: 1,
         flex: 1,
         justifyContent: 'center',
     },
-    text: {
+    wrapper: {
+        alignItems: 'center',
+        alignSelf: 'stretch',
+    },
+    icon: {
+        height: 65,
+        margin: 30,
+        width: 65,
+    },
+    h2: {
+        color: '#333333',
+        fontSize: 24,
+        margin: 10,
         textAlign: 'center',
+    },
+    h4: {
+        color: '#333333',
+        fontSize: 14,
+        margin: 10,
+        textAlign: 'center',
+        width: 210,
     },
 });
