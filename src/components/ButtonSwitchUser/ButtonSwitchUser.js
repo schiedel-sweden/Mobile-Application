746da34dfb88afba21d8f98fcf1367c9c2f686c3
Jeremy Danner <jeremydanner2@gmail.com>
2017-09-03 21:54:32 +0200

    added modal for switching user

--- src/components/ButtonSwitchUser/ButtonSwitchUser.js
+++ src/components/ButtonSwitchUser/ButtonSwitchUser.js
@@ -1,18 +1,47 @@
 import React from 'react';
-
+// https://facebook.github.io/react-native/docs/modal.html
 import {
+    Modal,
     Image,
     StyleSheet,
     TouchableOpacity,
+    View,
+    Text,
 } from 'react-native';
 
-export default class RetailersView extends React.Component {
+export default class ButtonSwitchUser extends React.Component {
+    state = {
+      modalVisible: false,
+    }
+
+    setModalVisible(visible) {
+      this.setState({modalVisible: visible});
+    }
+
     render() {
         return (
-            <TouchableOpacity style={styles.container} onPress={()=> alert('Växla användare')}>
-                {/* This image is a placeholeder for now  */}
-                <Image style={styles.img} source={require('../../images/icons/kontakta-oss.png')} />
-            </TouchableOpacity>
+            <View style={styles.container}>
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {alert("Modal has been closed.")}}
+                >
+                    <View style={{marginTop: 40}}>
+                        <TouchableOpacity onPress={() => {
+                             this.setModalVisible(!this.state.modalVisible)
+                        }}>
+                            <Text>Hide Modal</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
+
+                <TouchableOpacity onPress={() => {
+                        this.setModalVisible(!this.state.modalVisible)
+                }}>
+                    <Image style={styles.img} source={require('../../images/icons/kontakta-oss.png')} />
+                </TouchableOpacity>
+            </View>
         );
     }
 }
