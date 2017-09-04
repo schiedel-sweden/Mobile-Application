7f4fdd0ea55859976288440480613eb0780b4db7
Jeremy Danner <jeremydanner2@gmail.com>
2017-09-04 18:35:34 +0200

    added styles for buttons to switch users

--- src/components/ButtonSwitchUser/ButtonSwitchUser.js
+++ src/components/ButtonSwitchUser/ButtonSwitchUser.js
@@ -21,18 +21,31 @@ export default class ButtonSwitchUser extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Modal
-                    animationType="slide"
-                    transparent={false}
+                <Modal style={styles.modal}
+                    animationType="fade"
+                    transparent={true}
                     visible={this.state.modalVisible}
-                    onRequestClose={() => {alert("Modal has been closed.")}}
+                    onRequestClose={() => {}}
                 >
-                    <View style={{marginTop: 40}}>
-                        <TouchableOpacity onPress={() => {
+                    <View style={styles.modalContainer}>
+                        <TouchableOpacity style={styles.hideModalButton} onPress={() => {
                              this.setModalVisible(!this.state.modalVisible)
                         }}>
                             <Text>Hide Modal</Text>
                         </TouchableOpacity>
+
+                        <View style={styles.buttonsContainer}>
+                            <Text style={styles.h2}>Växla användare</Text>
+                            <TouchableOpacity style={styles.buttonSelectUser} onPress={() => {alert('button pressed!')}}>
+                                <Text style={styles.buttonText}>Återförsäljare</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.buttonSelectUser} onPress={() => {alert('button pressed!')}}>
+                                <Text style={styles.buttonText}>Montör</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.buttonSelectUser} onPress={() => {alert('button pressed!')}}>
+                                <Text style={styles.buttonText}>Privatperson</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </Modal>
 
@@ -61,5 +74,45 @@ const styles = StyleSheet.create({
     img: {
         height: 25,
         width: 25,
+    },
+    modal: {
+        // flex: 1,
+    },
+    modalContainer: {
+        alignSelf: 'stretch',
+        backgroundColor: '#B9B9B9',
+        flex: 1,
+        // opacity: 0.98,
+    },
+    hideModalButton: {
+        alignSelf: 'stretch',
+        right: 0,
+        top: 25,
+    },
+    buttonsContainer: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        marginTop: 50,
+    },
+    h2: {
+        color: '#333333',
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 60,
+    },
+    buttonSelectUser: {
+        backgroundColor: '#F9CE3C',
+        borderColor: '#333333',
+        borderRadius: 5,
+        borderWidth: 3,
+        marginBottom: 20,
+        marginTop: 20,
+        padding: 30,
+        width: 300,
+    },
+    buttonText: {
+        fontSize: 26,
+        textAlign: 'center',
     },
 });
