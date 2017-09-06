ae01325d238a96aca97512b411271c4852f7fb96
Jeremy Danner <jeremydanner2@gmail.com>
2017-09-06 14:28:23 +0200

    added fetch method for fetching users from a random api so we can wor with the real api in the future

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import {
-    View,
-    Text,
-    Image,
-    StyleSheet,
-} from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
-export default class Products extends React.Component {
+export default class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        }
+    }
+
+
+    state = {
+        data: []
+    }
+
+    componentWillMount(){
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        const response = await fetch('https://randomuser.me/api?results=1000');
+        // const response = await fetch('http://127.0.0.1:8000/api/excel');
+        const json = await response.json();
+        this.setState({data: json.results});
+        // this.setState({data: json});
+    }
 
     render() {
-        const { navigate } = this.props.navigation;
+        if (this.state.isLoading) {
+            this.state = {
+                isLoading: false
+            }
+            return (
+                <View style={{flex: 1, paddingTop: 20}}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }
         return (
-        <View style={styles.container}>
-            <Text>Produkter yo</Text>
-        </View>
+            <View style={{flex: 1, paddingTop: 20}}>
+                <FlatList
+                    data={this.state.data}
+                    KeyExtrctor={(x, i) => i}
+                    renderItem={({ item }) =>
+                    <Text>{`${item.name.first} ${item.name.last}`}</Text>}
+                />
+            </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
