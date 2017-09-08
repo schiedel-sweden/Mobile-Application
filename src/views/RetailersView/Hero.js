058ac85a8f16550c6756365558e8cf8c11045716
Jeremy Danner <jeremydanner2@gmail.com>
2017-09-08 10:59:31 +0200

    fixed text and styles for the ipad application

--- src/views/RetailersView/Hero.js
+++ src/views/RetailersView/Hero.js
@@ -11,7 +11,17 @@ export default class Hero extends React.Component {
         return(
             <View style={styles.container}>
                 <Image style={styles.img} source={require('./img/roof_image.png')} />
-                <Text style={styles.text}>Välkomsttext här</Text>
+                <View style={styles.logoWrapper}>
+                    <Text style={styles.logo}>SCHIEDEL</Text>
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.h1}>Välkomsttext här</Text>
+                    <Text style={styles.text}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </Text>
+                </View>
             </View>
         );
     }
@@ -28,14 +38,39 @@ const styles = StyleSheet.create({
         bottom: 0,
         flex: 1,
         left: 0,
-        opacity: 0.3,
         resizeMode: 'cover',
         right: 0,
         top: 0,
     },
-    text: {
-        backgroundColor: 'rgba(0,0,0,0)',
-        fontSize: 25,
+    logoWrapper: {
+        alignItems: 'center',
+        justifyContent: 'center',
         position: 'absolute',
-    }
+        top: 40,
+    },
+    logo: {
+        backgroundColor: 'rgba(0,0,0,0)',
+        justifyContent: 'center',
+    },
+    textContainer: {
+        backgroundColor: 'rgba(0,0,0,0)',
+        left: 100,
+        position: 'absolute',
+        top: 150,
+    },
+    h1: {
+        color: '#333333',
+        fontSize: 36,
+        left: 0,
+        marginBottom: 10,
+        top: 0,
+    },
+    text: {
+        color: '#333333',
+        fontSize: 14,
+        left: 0,
+        marginTop: 10,
+        top: 0,
+        width: 300,
+    },
 });
