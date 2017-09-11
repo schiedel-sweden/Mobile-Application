faac47e856169ecfbe4eb4842d5ef1ab12d70841
Jonathan Partain <jonathan.partain@gmail.com>
2017-09-11 19:30:36 +0200

    Getting data from local server, not showing up like it should. Jeremy will fix ;)

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, ListView, Text, View, StyleSheet } from 'react-native';
+import {
+    ActivityIndicator,
+    ListView,
+    Text,
+    View,
+    StyleSheet } from 'react-native';
+
 import axios from 'axios';
 
+
+var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
 export default class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isReady: false,
+            dataSource: ds,
+        }
+    }
+    componentWillMount() {
+        axios.get('https://api.dev/api/excel/10', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+
+        .then((response) => response.json())
+
+        .then((responseData) => {
+            this.setState({data: responseData, isReady: true});
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+        .done();
+
+    }
 
   render() {
-      console.log('start');
-      const url = 'https://api.dev/api/excel/12';
-      console.log(url);
-      axios.request(url)
-      .then((response) => {
-          console.log(response);
-          console.log('hej bög');
-      })
-      .catch(function (error) {
-          console.log(error);
-          console.log('då');
-      });
-      console.log('stop');
 
     return (
+
         <View style={styles.container}>
+            <Text>Pretext</Text>
+            <Text>{this.state.data[0]["material"]]}</Text>
             <Text>Hej</Text>
         </View>
     );
