5c9c590ff16e3d6b923c5225650139ddc03d27e4
Jeremy Danner <jeremydanner2@gmail.com>
2017-09-12 17:16:01 +0200

    added youtube comment

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, FlatList, Text } from 'react-native';
 import axios from 'axios';
-
+// this is the youtube video https://www.youtube.com/watch?v=IuYo009yc8w
 export default class Products extends Component {
     state = {
         data: [],
@@ -15,16 +15,20 @@ export default class Products extends Component {
         const response = await fetch('https://api.dev/api/excel/10');
         const json = await response.json();
         console.log(json);
-        this.setState({ data: json });
+        this.setState({ data: json[0] });
+        console.log(this.state);
     };
 
     render() {
         return (
             <View style={styles.container}>
+                {/* flatlist gets data from the state not sure if item[0] is right... */}
                 <FlatList
                     data={this.state.data}
                     keyExtractor={(x, i) => i}
-                    renderItem={({ item }) => <Text>{item[0]}</Text>}
+                    renderItem={({ item }) => (
+                        <Text>{`${item.id} ${item.material}`}</Text>
+                    )}
                 />
             </View>
         );
