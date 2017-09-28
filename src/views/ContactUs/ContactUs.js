906b48057d4d9d83ba69a0739cb3b85498175c5a
Jeremy Danner <jeremydanner2@gmail.com>
2017-09-28 21:09:41 +0200

    added basic styles and logic for contact form

--- src/views/ContactUs/ContactUs.js
+++ src/views/ContactUs/ContactUs.js
@@ -1,96 +1,115 @@
 import React from 'react';
-import {
-    View,
-    Text,
-    Image,
-    StyleSheet,
-    TextInput,
-    Button,
-} from 'react-native';
+import { View, Text, Image, StyleSheet, TextInput, Button } from 'react-native';
 
 import globalStyles from '../../styles/globalStyles';
 
 export default class ContactUs extends React.Component {
-
     constructor(props) {
         super(props);
-        this.state = { name: 'Ditt namn', email: 'Din email', message: 'Ditt meddelande', empty: '' };
-
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            empty: '',
+        };
     }
-
     handlePress() {
-        alert("pressed");
+        alert('pressed');
     }
-
-
     render() {
         const { navigate } = this.props.navigation;
 
         return (
-
             <View style={styles.container}>
                 <View>
-                    <Text style={[styles.text, globalStyles.h2]}>Kontakta oss</Text>
+                    <Text style={[styles.text, globalStyles.h2]}>
+                        Kontakta oss
+                    </Text>
                 </View>
 
                 <View>
                     <Text style={globalStyles.p}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
                     </Text>
                 </View>
 
                 <TextInput
-                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(name) => this.setState({name})}
-                    value={this.state.name}
-                 />
-                 <TextInput
-                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                     onChangeText={(email) => this.setState({email})}
-                     value={this.state.email}
-                  />
+                    style={styles.input}
+                    onChangeText={name => this.setState({ name })}
+                    placeholder="Ditt namn"
+                    autoCapitalize="words"
+                    returnKeyLabel="next"
+                    selectionColor="#F9CE3C"
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={email => this.setState({ email })}
+                    placeholder="Din epost adress"
+                    autoCapitalize="none"
+                    returnKeyLabel="next"
+                    selectionColor="#F9CE3C"
+                    keyboardType="email-address"
+                />
 
-                  <TextInput
-                      multiline={true}
-                      style={{height: 200, borderColor: 'gray', borderWidth: 1}}
-                      onChangeText={(message) => this.setState({email})}
-                      value={this.state.message}
-                   />
-
-
+                <TextInput
+                    multiline={true}
+                    style={styles.textAria}
+                    onChangeText={message => this.setState({ message })}
+                    placeholder="Ditt meddelande"
+                    returnKeyLabel="send"
+                    selectionColor="#F9CE3C"
+                />
 
                 <Button
                     title="Skicka"
-                    onPress={this.handlePress}/>
+                    onPress={this.handlePress}
+                    style={styles.button}
+                />
 
-                <View>
+                {/* <View>
                     <Image />
 
                     <Image />
 
                     <Image />
-                </View>
+                </View> */}
             </View>
         );
     }
-
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 20,
-
+        top: 45,
     },
-    icon: {
-        height: 24,
-        width: 24,
+    input: {
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#333333',
+        backgroundColor: '#fff',
+        padding: 15,
+        margin: 15,
     },
-    text: {
-        alignSelf: 'stretch',
-        textAlign: 'center',
+    textAria: {
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#333333',
+        backgroundColor: '#fff',
+        padding: 15,
+        margin: 15,
+        flex: 0.3,
+    },
+    button: {
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#333333',
+        backgroundColor: '#fff',
+        padding: 15,
+        margin: 15,
     },
 });
