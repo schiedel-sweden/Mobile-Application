ce924d95fbdc237a9f1836235658eeeb6720aa83
Jeremy Danner <jeremydanner2@gmail.com>
2017-10-02 10:36:41 +0200

    fixed styles

--- src/views/ContactUs/ContactUs.js
+++ src/views/ContactUs/ContactUs.js
@@ -7,7 +7,6 @@ import {
     TextInput,
     TouchableOpacity,
     KeyboardAvoidingView,
-    FlatList,
 } from 'react-native';
 import Header from '../../components/Header/Header';
 import globalStyles from '../../styles/globalStyles';
@@ -20,6 +19,7 @@ export default class ContactUs extends React.Component {
             email: '',
             message: '',
             empty: '',
+            ingress: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
         };
     }
     handlePress() {
@@ -29,59 +29,72 @@ export default class ContactUs extends React.Component {
         const { navigate } = this.props.navigation;
 
         return (
-            <FlatList>
-                <View style={styles.container}>
+            <View style={styles.container}>
+                <Header />
+                <KeyboardAvoidingView behavior="padding" style={styles.body}>
                     <View style={styles.headerContainer}>
-                        <Text style={globalStyles.h2}>Kontakta oss</Text>
+                        <View style={styles.headerTitle}>
+                            <Text style={globalStyles.h2}>Kontakta oss</Text>
+                        </View>
+                        <View style={styles.headerIngress}>
+                            <Text style={globalStyles.p}>
+                                {this.state.ingress}
+                            </Text>
+                        </View>
                     </View>
-
-                    <KeyboardAvoidingView
-                        behavior="padding"
-                        style={styles.ingressContainer}
-                    >
-                        <Text style={globalStyles.p}>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.
-                        </Text>
-                    </KeyboardAvoidingView>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={name => this.setState({ name })}
-                        placeholder="Ditt namn"
-                        autoCapitalize="words"
-                        returnKeyLabel="next"
-                        selectionColor="#F9CE3C"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={email => this.setState({ email })}
-                        placeholder="Din epost adress"
-                        autoCapitalize="none"
-                        returnKeyLabel="next"
-                        selectionColor="#F9CE3C"
-                        keyboardType="email-address"
-                    />
-
-                    <TextInput
-                        multiline={true}
-                        style={styles.textAria}
-                        onChangeText={message => this.setState({ message })}
-                        placeholder="Ditt meddelande"
-                        returnKeyLabel="send"
-                        selectionColor="#F9CE3C"
-                    />
-
-                    <TouchableOpacity
-                        onPress={this.handlePress}
-                        style={styles.button}
-                    >
-                        <Text>Skicka!</Text>
-                    </TouchableOpacity>
-                </View>
-            </FlatList>
+                    <View>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={name => this.setState({ name })}
+                            placeholder="Ditt namn"
+                            autoCapitalize="words"
+                            returnKeyLabel="next"
+                            selectionColor="#F9CE3C"
+                        />
+                    </View>
+                    <View>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={email => this.setState({ email })}
+                            placeholder="Din epost adress"
+                            autoCapitalize="none"
+                            returnKeyLabel="next"
+                            selectionColor="#F9CE3C"
+                            keyboardType="email-address"
+                        />
+                    </View>
+                    <View>
+                        <TextInput
+                            multiline={true}
+                            numberOfLines={8}
+                            style={styles.textAria}
+                            onChangeText={message => this.setState({ message })}
+                            placeholder="Ditt meddelande"
+                            returnKeyLabel="send"
+                            selectionColor="#F9CE3C"
+                        />
+                    </View>
+                    <View style={styles.buttonWrapper}>
+                        <TouchableOpacity
+                            onPress={this.handlePress}
+                            style={styles.button}
+                        >
+                            <Text>Skicka!</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.footerWrapper}>
+                        <View>
+                            <Text>IMAGE</Text>
+                        </View>
+                        <View>
+                            <Text>IMAGE</Text>
+                        </View>
+                        <View>
+                            <Text>IMAGE</Text>
+                        </View>
+                    </View>
+                </KeyboardAvoidingView>
+            </View>
         );
     }
 }
@@ -89,53 +102,60 @@ export default class ContactUs extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        top: 45,
         backgroundColor: '#B9B9B9',
     },
-    headerContainer: {
-        alignSelf: 'stretch',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 45,
+    body: {
+        padding: globalStyles.PADDING,
     },
-    ingressContainer: {
-        alignSelf: 'stretch',
+    headerContainer: {
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 15,
-        marginVertical: 15,
-        marginHorizontal: 45,
+    },
+    headerTitle: {
+        marginBottom: globalStyles.PADDING,
+    },
+    headerIngress: {
+        marginBottom: globalStyles.PADDING,
     },
     input: {
         borderRadius: 5,
         borderWidth: 2,
         borderColor: '#333333',
-        backgroundColor: '#fff',
-        padding: 15,
-        marginVertical: 15,
-        marginHorizontal: 45,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        backgroundColor: '#ffffff',
+        marginVertical: 10,
     },
     textAria: {
         borderRadius: 5,
         borderWidth: 2,
         borderColor: '#333333',
-        backgroundColor: '#fff',
-        padding: 15,
-        marginVertical: 15,
-        marginHorizontal: 45,
-        flex: 0.3,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        backgroundColor: '#ffffff',
+        marginVertical: 10,
+    },
+    buttonWrapper: {
+        justifyContent: 'flex-end',
+        flexDirection: 'row',
     },
     button: {
         borderRadius: 5,
         borderWidth: 2,
         borderColor: '#333333',
+        paddingVertical: 12,
+        paddingHorizontal: 17,
         backgroundColor: '#F9CE3C',
-        paddingHorizontal: 25,
-        paddingVertical: 20,
-        margin: 15,
-        width: 105,
-        alignSelf: 'center',
+        marginVertical: 10,
+        width: 100,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    footerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 30,
     },
 });
