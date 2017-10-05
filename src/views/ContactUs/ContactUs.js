1638f39ee04bb70729f382851aa2342e3df9ee4b
Jonathan Partain <jonathan.partain@gmail.com>
2017-10-05 17:55:19 +0200

    Added contact form, icons and text. Text has functions that opens map at correct place, calls telephone number end starts sending an email

--- src/views/ContactUs/ContactUs.js
+++ src/views/ContactUs/ContactUs.js
@@ -4,6 +4,7 @@ import {
     Text,
     Image,
     StyleSheet,
+    Linking,
     TextInput,
     TouchableOpacity,
     KeyboardAvoidingView,
@@ -11,9 +12,73 @@ import {
 import Header from '../../components/Header/Header';
 import globalStyles from '../../styles/globalStyles';
 
+import Communications from 'react-native-communications';
+
+
+const t = require('tcomb-form-native');
+const Form = t.form.Form;
+
+const ContactForm = t.struct({
+    name: t.String,
+    email: t.String,
+    message: t.String
+});
+
+// stylesheet overriding
+t.form.Form.stylesheet.textbox.normal.color = '#000000';
+t.form.Form.stylesheet.textbox.normal.backgroundColor = '#FFFFFF';
+t.form.Form.stylesheet.textbox.normal.borderColor = '#333333'
+t.form.Form.stylesheet.textbox.normal.borderWidth = 2;
+
+t.form.Form.stylesheet.textbox.normal.borderRadius = 5;
+
+t.form.Form.stylesheet.textbox.normal.marginVertical = 10;
+t.form.Form.stylesheet.textbox.normal.paddingVertical = 10;
+t.form.Form.stylesheet.textbox.normal.paddingHorizontal = 15;
+
+t.form.Form.stylesheet.textbox.error.color = '#000000';
+t.form.Form.stylesheet.textbox.error.backgroundColor = '#FFFFFF';
+t.form.Form.stylesheet.textbox.error.borderWidth = 2;
+
+t.form.Form.stylesheet.textbox.error.borderRadius = 5;
+
+t.form.Form.stylesheet.textbox.error.marginVertical = 10;
+t.form.Form.stylesheet.textbox.error.paddingVertical = 10;
+t.form.Form.stylesheet.textbox.error.paddingHorizontal = 15;
+
+
+// borderRadius: 5,
+// borderWidth: 2,
+// borderColor: '#333333',
+// paddingVertical: 10,
+// paddingHorizontal: 15,
+// backgroundColor: '#ffffff',
+// marginVertical: 10,
+
+
+const options = {
+    auto: 'none',
+    fields: {
+        name: {
+            placeholder: 'Ditt namn',
+            error: 'obligatoriskt!'
+
+        },
+        email: {
+            placeholder: 'Din e-mail',
+            error: 'obligatoriskt!'
+        },
+        message: {
+            placeholder: 'Ditt meddelande',
+            error: 'obligatoriskt!'
+        }
+    }
+};
+
 export default class ContactUs extends React.Component {
     constructor(props) {
         super(props);
+        this.handlePress = this.handlePress.bind(this);
         this.state = {
             name: '',
             email: '',
@@ -23,7 +88,9 @@ export default class ContactUs extends React.Component {
         };
     }
     handlePress() {
-        alert('pressed');
+        let value = this.refs.contactform.getValue();
+
+        alert(value.name + " " +value.email +" " + value.message);
     }
     render() {
         const { navigate } = this.props.navigation;
@@ -42,55 +109,51 @@ export default class ContactUs extends React.Component {
                             </Text>
                         </View>
                     </View>
-                    <View>
-                        <TextInput
-                            style={styles.input}
-                            onChangeText={name => this.setState({ name })}
-                            placeholder="Ditt namn"
-                            autoCapitalize="words"
-                            returnKeyLabel="next"
-                            selectionColor="#F9CE3C"
-                        />
-                    </View>
-                    <View>
-                        <TextInput
-                            style={styles.input}
-                            onChangeText={email => this.setState({ email })}
-                            placeholder="Din epost adress"
-                            autoCapitalize="none"
-                            returnKeyLabel="next"
-                            selectionColor="#F9CE3C"
-                            keyboardType="email-address"
-                        />
-                    </View>
-                    <View>
-                        <TextInput
-                            multiline={true}
-                            numberOfLines={8}
-                            style={styles.textAria}
-                            onChangeText={message => this.setState({ message })}
-                            placeholder="Ditt meddelande"
-                            returnKeyLabel="send"
-                            selectionColor="#F9CE3C"
-                        />
-                    </View>
+
+                    <Form
+                    ref="contactform"
+                    style={styles.input}
+                    type={ContactForm}
+                    options={options}
+
+                    />
                     <View style={styles.buttonWrapper}>
                         <TouchableOpacity
                             onPress={this.handlePress}
-                            style={styles.button}
-                        >
+                            style={styles.button} >
                             <Text>Skicka!</Text>
                         </TouchableOpacity>
                     </View>
                     <View style={styles.footerWrapper}>
                         <View>
-                            <Text>IMAGE</Text>
+                            <Image
+                            style={styles.largerIcons}
+                            source={require('../../images/icons/pin.png')}
+                             />
+                             <TouchableOpacity onPress={() => Linking.openURL('https://goo.gl/maps/CQu8tnGAzBL2')} >
+                             <Text>E A rosgrens gata 2z</Text>
+                             <Text>421 31 Västra Frölunda</Text>
+                             </TouchableOpacity>
+
+
                         </View>
                         <View>
-                            <Text>IMAGE</Text>
+                            <Image style={styles.largerIcons}
+                            source={require('../../images/icons/kontakt.png')}
+                            />
+                            <TouchableOpacity onPress={() => Communications.phonecall('013107050', true)} >
+                            <Text style={styles.yellow}>013 - 10 70 50</Text>
+                            </TouchableOpacity>
                         </View>
                         <View>
-                            <Text>IMAGE</Text>
+                            <Image
+                            style={styles.largerIcons}
+                            source={require('../../images/icons/email.png')}
+                            />
+                            <TouchableOpacity onPress={() => Communications.email('info@schiedel.se', '', '', '', '')} >
+                                <Text style={styles.yellow}>info@schiedel.se</Text>
+                            </TouchableOpacity>
+
                         </View>
                     </View>
                 </KeyboardAvoidingView>
@@ -162,4 +225,11 @@ const styles = StyleSheet.create({
         width: 24,
         height: 24,
     },
+    largerIcons: {
+        width: 48,
+        height: 48,
+    },
+    yellow: {
+        color: '#F9CE3C',
+    }
 });
