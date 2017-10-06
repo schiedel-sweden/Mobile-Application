fd8b7bfd71904b9a219681b6fbc47b040697b5e8
Jeremy Danner <jeremydanner2@gmail.com>
2017-10-06 11:50:52 +0200

    will fixe styles after design

--- src/views/PriceSuggestion/PriceSuggestion.js
+++ src/views/PriceSuggestion/PriceSuggestion.js
@@ -4,23 +4,23 @@ import {
     Text,
     Image,
     StyleSheet,
-    TouchableOpacity
+    TouchableOpacity,
+    ScrollView,
 } from 'react-native';
 import Header from '../../components/Header/Header';
 import globalStyles from '../../styles/globalStyles';
 
 // import local views
-import ChimneyType from './ChimneyType'
-import HouseType from './HouseType'
-import Chimney from './Chimney'
-import CustomerDetails from './CustomerDetails'
-import PricePage from './PricePage'
-import Order from './Order'
+import ChimneyType from './ChimneyType';
+import HouseType from './HouseType';
+import Chimney from './Chimney';
+import CustomerDetails from './CustomerDetails';
+import PricePage from './PricePage';
+import Order from './Order';
 
 import ButtonNav from './ButtonNav';
 
 export default class PriceSuggestion extends Component {
-
     constructor(props) {
         super(props);
         // bind all functions
@@ -34,50 +34,48 @@ export default class PriceSuggestion extends Component {
         this.quotationnumber = 192381;
         this.state = {
             activeTab: <Chimney />,
-            currentTab: "chimneytype",
-
-        }
-
+            currentTab: 'chimneytype',
+        };
     }
 
     // functions to set active tab to set tab file
-    chimneytype(){
+    chimneytype() {
         this.setState({
             activeTab: <ChimneyType />,
-            currentTab: "chimneytype",
-        })
-    };
+            currentTab: 'chimneytype',
+        });
+    }
     // pass offertnummer as the prop "offNum"
     housetype() {
         this.setState({
             activeTab: <HouseType quotNum={this.quotationnumber} />,
-            currentTab: "housetype"
-        })
-    };
+            currentTab: 'housetype',
+        });
+    }
     chimney() {
         this.setState({
             activeTab: <Chimney />,
-            currentTab: "chimney"
-        })
-    };
+            currentTab: 'chimney',
+        });
+    }
     customerdetails() {
         this.setState({
             activeTab: <CustomerDetails />,
-            currentTab: "customerdetails"
-        })
-    };
+            currentTab: 'customerdetails',
+        });
+    }
     pricepage() {
         this.setState({
             activeTab: <PricePage />,
-            currentTab: "pricepage"
-        })
-    };
+            currentTab: 'pricepage',
+        });
+    }
     order() {
         this.setState({
             activeTab: <Order />,
-            currentTab: "order"
-        })
-    };
+            currentTab: 'order',
+        });
+    }
 
     // 6 views!
 
@@ -88,96 +86,125 @@ export default class PriceSuggestion extends Component {
     // 5: PRISFÖRSLAG
     // 6: BESTÄLLNING
     render() {
-
         return (
             <View style={styles.container}>
+                <Header />
+                <ScrollView style={styles.body}>
+                    <View style={styles.navWrapper}>
+                        <View>
+                            <ButtonNav
+                                style={
+                                    this.state.currentTab == 'chimneytype'
+                                        ? styles.currentTab
+                                        : styles.tabItem
+                                }
+                                onPress={this.chimneytype}
+                                tagline="SKORSTENSTYP"
+                            />
+                        </View>
 
+                        <View>
+                            <ButtonNav
+                                style={
+                                    this.state.currentTab == 'housetype'
+                                        ? styles.currentTab
+                                        : styles.tabItem
+                                }
+                                onPress={this.housetype}
+                                tagline="HUSTYP"
+                            />
+                        </View>
 
-            <Header />
+                        <View>
+                            <ButtonNav
+                                style={
+                                    this.state.currentTab == 'chimney'
+                                        ? styles.currentTab
+                                        : styles.tabItem
+                                }
+                                onPress={this.chimney}
+                                tagline="SKORSTEN"
+                            />
+                        </View>
 
-                <View style={styles.body}>
+                        <View>
+                            <ButtonNav
+                                style={
+                                    this.state.currentTab == 'customerdetails'
+                                        ? styles.currentTab
+                                        : styles.tabItem
+                                }
+                                onPress={this.customerdetails}
+                                tagline="KUNDUPGIFTER"
+                            />
+                        </View>
 
-                    <View>
-                        <ButtonNav
-                        style={this.state.currentTab == "chimneytype" ? styles.currentTab : styles.tabItem}
-                        onPress={this.chimneytype}
-                        tagline="SKORSTENSTYP" />
+                        <View>
+                            <ButtonNav
+                                style={
+                                    this.state.currentTab == 'pricepage'
+                                        ? styles.currentTab
+                                        : styles.tabItem
+                                }
+                                onPress={this.pricepage}
+                                tagline="PRISFÖRSLAG"
+                            />
+                        </View>
+
+                        <View>
+                            <ButtonNav
+                                style={
+                                    this.state.currentTab == 'order'
+                                        ? styles.currentTab
+                                        : styles.tabItem
+                                }
+                                onPress={this.order}
+                                tagline="BESTÄLLNING"
+                            />
+                        </View>
                     </View>
-
-                    <View>
-                        <ButtonNav
-                        style={this.state.currentTab == "housetype" ? styles.currentTab: styles.tabItem}
-                        onPress={this.housetype}
-                        tagline="HUSTYP" />
-                    </View>
-
-                    <View>
-                        <ButtonNav
-                        style={this.state.currentTab == "chimney" ? styles.currentTab : styles.tabItem}
-                        onPress={this.chimney}
-                        tagline="SKORSTEN" />
-                    </View>
-
-                    <View>
-                        <ButtonNav
-                        style={this.state.currentTab == "customerdetails" ? styles.currentTab : styles.tabItem}
-                        onPress={this.customerdetails}
-                        tagline="KUNDUPGIFTER" />
-                    </View>
-
-                    <View>
-                        <ButtonNav
-                        style={this.state.currentTab == "pricepage" ? styles.currentTab : styles.tabItem}
-                        onPress={this.pricepage}
-                        tagline="PRISFÖRSLAG" />
-                    </View>
-
-                    <View>
-                        <ButtonNav
-                        style={this.state.currentTab == "order" ? styles.currentTab : styles.tabItem}
-                        onPress={this.order}
-                        tagline="BESTÄLLNING" />
-                    </View>
-
-                </View>
-
-
-                <View>
-                    {this.state.activeTab}
-                </View>
-
-
-
+                    <View>{this.state.activeTab}</View>
+                </ScrollView>
             </View>
-
         );
     }
 }
-
-
-
-
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
     body: {
-        justifyContent: 'space-between',
-        flexDirection: 'row',
+        backgroundColor: '#EEEEEE',
     },
-    separate: {
-        paddingTop: 20,
-        paddingBottom: 20,
+    navWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignSelf: 'stretch',
+        height: 75,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#949494',
     },
     tabItem: {
-        alignItems: 'center',
-        height: 100,
-        backgroundColor: "#FF0000"
+        // alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        borderWidth: 2,
+        borderColor: '#333333',
+        // alignItems: 'stretch',
+        // flexGrow: 1,
+        flex: 1,
     },
     currentTab: {
-        alignItems: 'center',
-        height: 100,
-        backgroundColor: "#00FF00"
-    }
+        // alignItems: 'center',
+        backgroundColor: '#EEEEEE',
+        justifyContent: 'center',
+        height: '100%',
+        // alignItems: 'stretch',
+        // borderWidth: 3,
+        // borderColor: '#333333',
+        // flexGrow: 1,
+        flex: 1,
+    },
 });
