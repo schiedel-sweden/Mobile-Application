f953a03b1072f8c26570547c19e7f66feed49177
Asmatkhan <jilor-man@hotmail.com>
2017-10-14 10:28:30 +0200

    Separate dropdown child from parent, and have it own design

--- src/views/Products/TouchableBlock.js
+++ src/views/Products/TouchableBlock.js
@@ -19,37 +19,44 @@ export default class TouchableBlock extends Component {
         }
         this.setVisibleMat = this.setVisibleMat.bind(this);
     }
+
     /**
     * @return bool setState
     */
     setVisibleMat() {
         this.setState({visible: !this.state.visible});
-    };
+    }
 
     /**
     * @return View
     */
     render() {
         return (
-            <TouchableOpacity onPress={this.setVisibleMat} style={[styles.container, bGColor(this.props.matTopic.id)]}>
+          <View>
+            <TouchableOpacity onPress={this.setVisibleMat}>
+              <View style={[styles.container,bGSwitchingColor(this.props.matTopic.id)]}>
                 <View>
                     <Text style={globalStyles.h1}>{this.props.matTopic.tagline}</Text>
                 </View>
                 <View>
                     <Text style={globalStyles.h3}>{this.props.matTopic.ingress}</Text>
                 </View>
-                {this.state.visible && (
-                    <View style={styles.dropDownContainer}>
-                        {this.props.mat.map(function(object, i) {
-                            return (
-                                <Text style={styles.dropDownText} key={i}>
-                                    {object}
-                                </Text>
-                            );
-                        })}
-                    </View>
-                )}
-            </TouchableOpacity>
+              </View>
+          </TouchableOpacity>
+          {this.state.visible && (
+            <View>
+                {this.props.mat.map(function(object, i) {
+                    return (
+                      <View style={styles.dropDownContainer} key={i}>
+                        <Text style={styles.dropDownText} key={i}>
+                            {object}
+                        </Text>
+                      </View>
+                    );
+                })}
+            </View>
+        )}
+        </View>
         );
     }
 }
@@ -58,32 +65,35 @@ export default class TouchableBlock extends Component {
 * @param props.matTopic.id
 * @return styles
 */
-function bGColor(id){
+function bGSwitchingColor(id){
   if (id % 2 == 0) {
     return styles.lightBackground;
   } else {
     return styles.darkBackground;
   }
-}
+};
 
 const styles = StyleSheet.create({
     container: {
         alignSelf: 'stretch',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 1,
-        borderWidth: 1,
-        borderColor: '#B9B9B9',
         padding: globalStyles.PADDING,
     },
     lightBackground: {
       backgroundColor: '#EEEEEE',
+      borderRadius: 1,
+      borderWidth: 1,
+      borderColor: '#B9B9B9',
     },
     darkBackground: {
       backgroundColor: '#B9B9B9',
     },
     dropDownContainer: {
-        paddingHorizontal: globalStyles.PADDING,
+      padding: globalStyles.PADDING,
+      borderRadius: 1,
+      borderWidth: 1,
+      borderColor: '#B9B9B9',
     },
     dropDownText: {
         textAlign: 'left',
