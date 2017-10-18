06735386a1dc5a8597a62e3fee5938ded6e11d1f
Asmatkhan <jilor-man@hotmail.com>
2017-10-18 13:28:53 +0200

    adding temporary look to ’MatarialDetail’ and delete ’mat2’, ’mat3’ in Products

--- src/views/Products/MaterialDetail.js
+++ src/views/Products/MaterialDetail.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import {
     View,
+    TouchableOpacity,
     StyleSheet,
     Text,
+    Image,
 } from 'react-native';
 import globalStyles from '../../styles/globalStyles';
+import navStyles from '../../styles/navStyles';
 
 export default class MaterialDetail extends Component {
     /**
@@ -13,7 +16,46 @@ export default class MaterialDetail extends Component {
     */
     constructor(props) {
         super(props);
+        this.state = {
+            visible: false,
+        }
+        this.setVisibleMat = this.setVisibleMat.bind(this);
+        this.listItem = this.listItem.bind(this);
     }
+
+    /**
+    * @return bool setState
+    */
+    setVisibleMat() {
+        this.setState({visible: !this.state.visible});
+    }
+    /**
+    * @return Image
+    */
+    getArrowIcon(){
+        const arrowIcon = this.state.visible
+            ? <Image style={[navStyles.icon, styles.icon]}
+                     source={require('./img/arrow_opened.png')}
+              />
+            : <Image style={[navStyles.icon, styles.icon]}
+                     source={require('./img/arrow.png')}
+              />;
+        return arrowIcon;
+    }
+    /**
+    * @return listMaterial[...]
+    */
+    listItem() {
+        const listItem = <View>
+                            <Text style={globalStyles.h4}>
+                              Bruksområde
+                            </Text>
+                            <Text style={globalStyles.h4}>
+                              Bruksområde
+                            </Text>
+                        </View>
+        return listItem;
+    };
 
     /**
     * @return View
@@ -21,37 +63,76 @@ export default class MaterialDetail extends Component {
     render() {
         return (
           <View >
-              <Text style={[globalStyles.p, styles.text]}>
-                  djshaf adskjfna fd askfj asd dsjf dsfjh dsaf jidsf öasfdhkhk
-              </Text>
+              <Image
+                  style={styles.img}
+                  source={require('./img/roof.png')}
+              />
+              <View style={styles.container}>
+                  <Text style={[globalStyles.h2, styles.topic]}>
+                      Solid vent
+                  </Text>
+                  <Text style={[globalStyles.h4, styles.ingress]}>
+                      Solid vent
+                  </Text>
+                  <Text style={[globalStyles.p, styles.text]}>
+                      djshaf adskjfna fd askfj asd dsjf dsfjh dsaf jidsf öasfdhkhk
+                  </Text>
+                  <Text style={[globalStyles.h4, styles.ingress]}>
+                      Solid vent
+                  </Text>
+              </View>
+              <TouchableOpacity onPress={this.setVisibleMat}>
+                  <View style={styles.touchableContainer}>
+                      <View style={styles.touchableItem}>
+                          <Text style={globalStyles.h4}>
+                              Bruksområde                 {this.getArrowIcon()}
+                          </Text>
+                      </View>
+                  </View>
+              </TouchableOpacity>
+              {this.state.visible && (this.listItem())}
           </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    containerLightBackground: {
-        backgroundColor: '#EEEEEE',
+    container: {
+        alignSelf: 'stretch',
         padding: globalStyles.PADDING,
-        borderRadius: 1,
-        borderWidth: 1,
-        borderColor: '#B9B9B9',
     },
-    containerDarkBackground: {
+    touchableContainer: {
         backgroundColor: '#B9B9B9',
-        padding: globalStyles.PADDING,
         borderRadius: 1,
         borderWidth: 1,
         borderColor: '#EEEEEE',
+        paddingHorizontal: globalStyles.PADDING,
+        paddingVertical: globalStyles.PADDING * 0.25,
+    },
+    touchableItem: {
+        paddingHorizontal: globalStyles.PADDING,
+    },
+    topic: {
+        paddingHorizontal: globalStyles.PADDING,
+        paddingBottom: globalStyles.PADDING * 0.5,
+    },
+    ingress: {
+        paddingHorizontal: globalStyles.PADDING,
+        paddingBottom: globalStyles.PADDING,
     },
     text: {
         paddingHorizontal: globalStyles.PADDING,
         paddingBottom: globalStyles.PADDING,
         textAlign: 'left',
     },
-    link: {
-        color: '#F9CE3C',
-        paddingHorizontal: globalStyles.PADDING,
-        textAlign: 'left',
+    img: {
+        resizeMode: 'cover',
+        flex: 1,
+        width: globalStyles.DEVICE_WIDTH,
+        height: globalStyles.DEVICE_HEIGHT * 0.22,
     },
+    icon: {
+        alignSelf: 'flex-end',
+    },
+
 });
--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -23,12 +23,8 @@ export default class Products extends Component {
             matTopics: [{id: 0, tagline: "Toggle material 1 items", ingress: "material one"},
                         {id: 1, tagline: "Toggle material 2 items", ingress: "material two"},
                         {id: 2, tagline: "Toggle material 3 items", ingress: "material tree"}],
-            mat1: [],
-            mat2: [],
-            mat3: [],
+            mat: [],
             visible1: true,
-            visible2: true,
-            visible3: true,
             data: [],
             refreshing: false,
             isMaterialDetailViewActived: false,
@@ -89,30 +85,18 @@ export default class Products extends Component {
     getMaterial() {
         let ids = [];
         // material "144029"
-        let mat1Arr = [];
-        // material "148918"
-        let mat2Arr = [];
-        // material "144026"
-        let mat3Arr = [];
+        let matArr = [];
 
         for (let i = 0; i < this.state.data.length; i++) {
-            // add items to each material array
+            // add items to material array
             if (i < 5) {
-                mat1Arr.push(this.state.data[i].designation);
-            }
-            if (i >= 5 && i < 20) {
-                mat2Arr.push(this.state.data[i].designation);
-            }
-            if (i >= 20) {
-                mat3Arr.push(this.state.data[i].designation);
+                matArr.push(this.state.data[i].designation);
             }
 
             ids.push(this.state.data[i].id);
         }
         this.setState({
-            mat1: mat1Arr,
-            mat2: mat2Arr,
-            mat3: mat3Arr,
+            mat: matArr,
         });
     }
     /**
@@ -122,7 +106,7 @@ export default class Products extends Component {
         const listTouchableBlock = this.state.matTopics.map((topic, i) =>
             <TouchableBlock key = {i} matTopic={topic}
             visible={this.state.visible1}
-            mat={this.state.mat1}
+            mat={this.state.mat}
             navigation={this.props.navigation}
             order={i}
             setActiveMaterialDetailView = {this.setActiveMaterialDetailView}/>
